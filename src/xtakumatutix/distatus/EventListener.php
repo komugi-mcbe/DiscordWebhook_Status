49222c44e137a6d82fac0415ce38cc67b5901d40
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-05-20 22:06:23 +0900

    configで指定するようにした＆起動メッセージ書き方変えた

--- src/xtakumatutix/distatus/EventListener.php
+++ src/xtakumatutix/distatus/EventListener.php
@@ -7,36 +7,46 @@ namespace xtakumatutix\distatus;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\utils\Config;
 use bbo51dog\pmdiscord\Sender;
 use bbo51dog\pmdiscord\element\Content;
 
 class EventListener implements Listener 
 {
-	public function onJoin(PlayerJoinEvent $event)
-	{
-		$player = $event->getPlayer();
-		$name = $player->getName();
-    	$date = date("G:i");
+    private $Main;
+
+    public function _construct(Main $Main)
+    {
+        $Main = $this->Main;
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $token = $Main->config->get('token');
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $date = date("G:i");
         $content = new Content();
         $content->setText(">> Join ".$name." \n Time > ".$date);
-        $webhook = Sender::create("")
+        $webhook = Sender::create($token)
             ->add($content)
             ->setCustomName("Join")
             ->setCustomAvatar("https://user-images.githubusercontent.com/47268002/82444822-c236a600-9ade-11ea-8cbf-a2725896f7a5.png");
         Sender::send($webhook);
-	}
+    }
 
-	public function onQuit(PlayerQuitEvent $event)
-	{
-		$player = $event->getPlayer();
-		$name = $player->getName();
-    	$date = date("G:i");
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $token = $Main->config->get('token');
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $date = date("G:i");
         $content = new Content();
         $content->setText(">> Quit ".$name." \n Time > ".$date);
-        $webhook = Sender::create("")
+        $webhook = Sender::create($token)
             ->add($content)
             ->setCustomName("Quit")
             ->setCustomAvatar("https://user-images.githubusercontent.com/47268002/82444815-c1057900-9ade-11ea-8671-1e8587d56b18.png");
         Sender::send($webhook);
-	}
+    }
 }
--- src/xtakumatutix/distatus/Main.php
+++ src/xtakumatutix/distatus/Main.php
@@ -6,20 +6,25 @@ namespace xtakumatutix\distatus;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
+use pocketmine\utils\Config;
 use xtakumatutix\distatus\EventListener;
 use bbo51dog\pmdiscord\Sender;
 use bbo51dog\pmdiscord\element\Content;
-
 
 Class Main extends PluginBase 
 {
     public function onEnable() 
     {
-    	$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-    	$date = date("G:i");
+        $this->getLogger()->notice("起動メッセージを送信しました - ver.".$this->getDescription()->getVersion());
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML, [
+            'token' => 'https://discordapp.com/api/webhooks/00000/xxxxx'
+        ]);
+        $token = $this->config->get('token');
+        $date = date("G:i");
         $content = new Content();
         $content->setText(">> Open \n Time > ".$date);
-        $webhook = Sender::create("")
+        $webhook = Sender::create($token)
             ->add($content)
             ->setCustomName("ServerOpen")
             ->setCustomAvatar("https://user-images.githubusercontent.com/47268002/82444806-bea31f00-9ade-11ea-9181-0e6b5a3fe254.png");
@@ -28,13 +33,15 @@ Class Main extends PluginBase
 
     public function onDisable()
     {
-    	$date = date("G:i");
+        $this->getLogger()->notice("停止メッセージを送信しました");
+        $token = $this->config->get('token');
+        $date = date("G:i");
         $content = new Content();
         $content->setText(">> Stop \n Time > ".$date);
-        $webhook = Sender::create("")
+        $webhook = Sender::create($token)
             ->add($content)
             ->setCustomName("ServerStop")
             ->setCustomAvatar("https://user-images.githubusercontent.com/47268002/82444811-bfd44c00-9ade-11ea-9feb-27d78310903e.png");
-        Sender::send($webhook); 	
+        Sender::send($webhook); 
     }
 }
