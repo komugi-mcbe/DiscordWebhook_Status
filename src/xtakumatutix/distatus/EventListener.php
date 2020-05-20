f3c4a14d976b9e92f6dab7a461e12f64f5fa034c
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-05-20 21:43:45 +0900

    token漏れ thank @tomo0611

--- src/xtakumatutix/distatus/EventListener.php
+++ src/xtakumatutix/distatus/EventListener.php
@@ -19,7 +19,7 @@ class EventListener implements Listener
     	$date = date("G:i");
         $content = new Content();
         $content->setText(">> Join ".$name." \n Time > ".$date);
-        $webhook = Sender::create("https://discordapp.com/api/webhooks/712400360014086217/N7ckUq7lTqUzbPe8Tefc7rkVTeg-K09EqOE1Rib0N1-wGUb_ArC0AoJFYwe7WPdyxad2")
+        $webhook = Sender::create("")
             ->add($content)
             ->setCustomName("Join")
             ->setCustomAvatar("https://user-images.githubusercontent.com/47268002/82444822-c236a600-9ade-11ea-8cbf-a2725896f7a5.png");
@@ -33,7 +33,7 @@ class EventListener implements Listener
     	$date = date("G:i");
         $content = new Content();
         $content->setText(">> Quit ".$name." \n Time > ".$date);
-        $webhook = Sender::create("https://discordapp.com/api/webhooks/712400360014086217/N7ckUq7lTqUzbPe8Tefc7rkVTeg-K09EqOE1Rib0N1-wGUb_ArC0AoJFYwe7WPdyxad2")
+        $webhook = Sender::create("")
             ->add($content)
             ->setCustomName("Quit")
             ->setCustomAvatar("https://user-images.githubusercontent.com/47268002/82444815-c1057900-9ade-11ea-8671-1e8587d56b18.png");
